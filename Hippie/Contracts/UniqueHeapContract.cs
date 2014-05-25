9d1c5d2255704b51979ee861ce07265e91b01192
Alessio Parma <alessio.parma@gmail.com>
2014-05-25 08:43:44 +0200

    New repo

---
+++ Hippie/Contracts/UniqueHeapContract.cs
@@ -0,0 +1,160 @@
+﻿// 
+// UniqueHeapContract.cs
+//  
+// Author:
+//       Alessio Parma <alessio.parma@gmail.com>
+// 
+// Copyright (c) 2012-2014 Alessio Parma <alessio.parma@gmail.com>
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+namespace Hippie.Contracts
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+
+    [ContractClassFor(typeof(IHeap<,>))]
+    abstract class UniqueHeapContract<TVal, TPr> : IHeap<TVal, TPr>
+    {
+        public TPr this[TVal val]
+        {
+            get
+            {
+                Contract.Requires<ArgumentException>(Contains(val), ErrorMessages.NoValue);
+                Contract.Ensures(Count == Contract.OldValue(Count));
+                Contract.Ensures(!ReferenceEquals(Contract.Result<TPr>(), null));
+                Contract.Ensures(Contains(val, Contract.Result<TPr>()));
+                return default(TPr);
+            }
+            set
+            {
+                // Priority should never be null to avoid exceptions while using comparers.
+                Contract.Requires<ArgumentNullException>(!ReferenceEquals(value, null), ErrorMessages.NullPriority);
+                Contract.Requires<ArgumentException>(Contains(val), ErrorMessages.NoValue);
+                Contract.Ensures(Count == Contract.OldValue(Count));
+                Contract.Ensures(Contains(val, value));
+                Contract.Ensures(Comparer.Compare(this[val], value) == 0);
+            }
+        }
+
+        public bool Contains(TVal value)
+        {
+            Contract.Ensures(Count == Contract.OldValue(Count));
+            return default(bool);
+        }
+
+        public bool Contains(TVal value, TPr priority)
+        {
+            Contract.Ensures(Count == Contract.OldValue(Count));
+            return default(bool);
+        }
+
+        public TPr PriorityOf(TVal value)
+        {
+            Contract.Requires<ArgumentException>(Contains(value), ErrorMessages.NoValue);
+            Contract.Ensures(Count == Contract.OldValue(Count));
+            Contract.Ensures(!ReferenceEquals(Contract.Result<TPr>(), null));
+            Contract.Ensures(Contains(value, Contract.Result<TPr>()));
+            return default(TPr);
+        }
+
+        public IHeapHandle<TVal, TPr> Remove(TVal value)
+        {
+            Contract.Requires<ArgumentException>(Contains(value), ErrorMessages.NoValue);
+            Contract.Ensures(Count == Contract.OldValue(Count) - 1);
+            Contract.Ensures(Contract.Result<IHeapHandle<TVal, TPr>>() != null);
+            Contract.Ensures(!Contains(value) && !Contains(value, Contract.Result<IHeapHandle<TVal, TPr>>().Priority));
+// ReSharper disable AssignNullToNotNullAttribute
+            return default(IHeapHandle<TVal, TPr>);
+// ReSharper restore AssignNullToNotNullAttribute
+        }
+
+        public TPr Update(TVal value, TVal newValue, TPr newPriority)
+        {
+            // Differently from raw heaps, here value should never be null,
+            // in order to avoid exceptions while using equality comparers.
+            Contract.Requires<ArgumentNullException>(!ReferenceEquals(value, null), ErrorMessages.NullValue);
+            // Priority should never be null to avoid exceptions while using comparers.
+            Contract.Requires<ArgumentNullException>(!ReferenceEquals(newValue, null), ErrorMessages.NullPriority);
+            Contract.Requires<ArgumentException>(Contains(value), ErrorMessages.NoValue);
+            Contract.Requires<ArgumentException>(EqualityComparer.Equals(value, newValue) || !Contains(newValue),
+                                                 ErrorMessages.ValueContained);
+            Contract.Ensures(Count == Contract.OldValue(Count));
+            Contract.Ensures(!ReferenceEquals(Contract.Result<TPr>(), null));
+            Contract.Ensures(Contains(newValue) && Contains(newValue, newPriority));
+            Contract.Ensures(EqualityComparer.Equals(value, newValue) || !Contains(value));
+            Contract.Ensures(EqualityComparer.Equals(value, newValue) || !Contains(value, Contract.Result<TPr>()));
+            return default(TPr);
+        }
+
+        public TPr UpdatePriorityOf(TVal value, TPr newPriority)
+        {
+            // Priority should never be null to avoid exceptions while using comparers.
+            Contract.Requires<ArgumentNullException>(!ReferenceEquals(newPriority, null), ErrorMessages.NullPriority);
+            Contract.Requires<ArgumentException>(Contains(value), ErrorMessages.NoValue);
+            Contract.Ensures(Count == Contract.OldValue(Count));
+            Contract.Ensures(Contains(value, newPriority));
+            Contract.Ensures(Comparer.Compare(this[value], newPriority) == 0);
+            Contract.Ensures(Comparer.Compare(Contract.Result<TPr>(), newPriority) == 0 ||
+                             !Contains(value, Contract.Result<TPr>()));
+            return default(TPr);
+        }
+
+        public void UpdateValue(TVal value, TVal newValue)
+        {
+            // Differently from raw heaps, here value should never be null,
+            // in order to avoid exceptions while using equality comparers.
+            Contract.Requires<ArgumentNullException>(!ReferenceEquals(newValue, null), ErrorMessages.NullValue);
+            Contract.Requires<ArgumentException>(Contains(value), ErrorMessages.NoValue);
+            Contract.Requires<ArgumentException>(EqualityComparer.Equals(value, newValue) || !Contains(newValue),
+                                                 ErrorMessages.ValueContained);
+            Contract.Ensures(Count == Contract.OldValue(Count));
+            Contract.Ensures(Contains(newValue));
+            Contract.Ensures(EqualityComparer.Equals(value, newValue) || !Contains(value));
+        }
+
+        #region IThinHeap Members
+
+        public abstract int Count { get; }
+        public abstract bool IsReadOnly { get; }
+        public abstract void Add(IHeapHandle<TVal, TPr> item);
+        public abstract void Clear();
+        public abstract bool Contains(IHeapHandle<TVal, TPr> item);
+        public abstract void CopyTo(IHeapHandle<TVal, TPr>[] array, int arrayIndex);
+        public abstract IEnumerator<IHeapHandle<TVal, TPr>> GetEnumerator();
+        public abstract bool Remove(IHeapHandle<TVal, TPr> item);
+
+        public abstract IComparer<TPr> Comparer { get; }
+        public abstract IEqualityComparer<TVal> EqualityComparer { get; }
+        public abstract IHeapHandle<TVal, TPr> Min { get; }
+        public abstract void Add(TVal val, TPr priority);
+        public abstract void Merge<TVal2, TPr2>(IThinHeap<TVal2, TPr2> other) where TVal2 : TVal where TPr2 : TPr;
+        public abstract IHeapHandle<TVal, TPr> RemoveMin();
+        public abstract IEnumerable<IReadOnlyTree<TVal, TPr>> ToReadOnlyForest();
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+    }
+}
---
+++ Hippie/Core/ReadOnlyTree.cs
@@ -0,0 +1,271 @@
+﻿// 
+// ReadOnlyTree.cs
+//  
+// Author:
+//       Alessio Parma <alessio.parma@gmail.com>
+// 
+// Copyright (c) 2012-2014 Alessio Parma <alessio.parma@gmail.com>
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+namespace Hippie.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    sealed class ReadOnlyTree<TVal, TPr> : IReadOnlyTree<TVal, TPr>
+    {
+        readonly List<ReadOnlyTree<TVal, TPr>> _children = new List<ReadOnlyTree<TVal, TPr>>();
+        readonly ReadOnlyTree<TVal, TPr> _parent;
+        readonly TPr _priority;
+        readonly TVal _value;
+
+        public ReadOnlyTree(TVal value, TPr priority, ReadOnlyTree<TVal, TPr> parent)
+        {
+            _value = value;
+            _priority = priority;
+            _parent = parent;
+            if (parent != null) {
+                parent._children.Add(this);
+            }
+        }
+
+        public TVal Value
+        {
+            get { return _value; }
+        }
+
+        public TPr Priority
+        {
+            get { return _priority; }
+        }
+
+        public IReadOnlyTree<TVal, TPr> Parent
+        {
+            get { return _parent; }
+        }
+
+        public IEnumerable<IReadOnlyTree<TVal, TPr>> Children
+        {
+            get { return _children; }
+        }
+
+        public IEnumerable<IReadOnlyTree<TVal, TPr>> BreadthFirstVisit()
+        {
+            var queue = new Queue<ReadOnlyTree<TVal, TPr>>();
+            queue.Enqueue(this);
+            while (queue.Count > 0) {
+                var t = queue.Dequeue();
+                yield return t;
+                foreach (var c in t._children) {
+                    queue.Enqueue(c);
+                }
+            }
+        }
+
+        public void BreadthFirstVisit(Action<IReadOnlyTree<TVal, TPr>> visitor)
+        {
+            var queue = new Queue<ReadOnlyTree<TVal, TPr>>();
+            queue.Enqueue(this);
+            while (queue.Count > 0) {
+                var t = queue.Dequeue();
+                visitor(t);
+                foreach (var c in t._children) {
+                    queue.Enqueue(c);
+                }
+            }
+        }
+
+        public IEnumerable<TRet> BreadthFirstVisit<TRet>(Func<IReadOnlyTree<TVal, TPr>, TRet, TRet> visitor, TRet start)
+        {
+            var queue = new Queue<Pair<ReadOnlyTree<TVal, TPr>, TRet>>();
+            queue.Enqueue(Pair.Create(this, start));
+            while (queue.Count > 0) {
+                var tuple = queue.Dequeue();
+                var res = visitor(tuple.First, tuple.Second);
+                yield return res;
+                foreach (var c in tuple.First._children) {
+                    queue.Enqueue(Pair.Create(c, res));
+                }
+            }
+        }
+
+        public IEnumerable<IReadOnlyTree<TVal, TPr>> DepthFirstVisit()
+        {
+            var stack = new Stack<ReadOnlyTree<TVal, TPr>>();
+            stack.Push(this);
+            while (stack.Count > 0) {
+                var t = stack.Pop();
+                yield return t;
+                foreach (var c in t._children) {
+                    stack.Push(c);
+                }
+            }
+        }
+
+        public void DepthFirstVisit(Action<IReadOnlyTree<TVal, TPr>> visitor)
+        {
+            var stack = new Stack<ReadOnlyTree<TVal, TPr>>();
+            stack.Push(this);
+            while (stack.Count > 0) {
+                var t = stack.Pop();
+                visitor(t);
+                foreach (var c in t._children) {
+                    stack.Push(c);
+                }
+            }
+        }
+
+        public IEnumerable<TRet> DepthFirstVisit<TRet>(Func<IReadOnlyTree<TVal, TPr>, TRet, TRet> visitor, TRet start)
+        {
+            var stack = new Stack<Pair<ReadOnlyTree<TVal, TPr>, TRet>>();
+            stack.Push(Pair.Create(this, start));
+            while (stack.Count > 0) {
+                var tuple = stack.Pop();
+                var res = visitor(tuple.First, tuple.Second);
+                yield return res;
+                foreach (var c in tuple.First._children) {
+                    stack.Push(Pair.Create(c, res));
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[Value: {0}; Priority: {1}; Children: {2}]", Value, Priority, Children.Count());
+        }
+    }
+
+    sealed class ReadOnlyTree<T> : IReadOnlyTree<T>
+    {
+        readonly List<ReadOnlyTree<T>> _children = new List<ReadOnlyTree<T>>();
+        readonly T _item;
+        readonly ReadOnlyTree<T> _parent;
+
+        public ReadOnlyTree(T item, ReadOnlyTree<T> parent)
+        {
+            _item = item;
+            _parent = parent;
+            if (parent != null) {
+                parent._children.Add(this);
+            }
+        }
+
+        public T Item
+        {
+            get { return _item; }
+        }
+
+        public IReadOnlyTree<T> Parent
+        {
+            get { return _parent; }
+        }
+
+        public IEnumerable<IReadOnlyTree<T>> Children
+        {
+            get { return _children; }
+        }
+
+        public IEnumerable<IReadOnlyTree<T>> BreadthFirstVisit()
+        {
+            var queue = new Queue<ReadOnlyTree<T>>();
+            queue.Enqueue(this);
+            while (queue.Count > 0) {
+                var t = queue.Dequeue();
+                yield return t;
+                foreach (var c in t._children) {
+                    queue.Enqueue(c);
+                }
+            }
+        }
+
+        public void BreadthFirstVisit(Action<IReadOnlyTree<T>> visitor)
+        {
+            var queue = new Queue<ReadOnlyTree<T>>();
+            queue.Enqueue(this);
+            while (queue.Count > 0) {
+                var t = queue.Dequeue();
+                visitor(t);
+                foreach (var c in t._children) {
+                    queue.Enqueue(c);
+                }
+            }
+        }
+
+        public IEnumerable<TRet> BreadthFirstVisit<TRet>(Func<IReadOnlyTree<T>, TRet, TRet> visitor, TRet start)
+        {
+            var queue = new Queue<Pair<ReadOnlyTree<T>, TRet>>();
+            queue.Enqueue(Pair.Create(this, start));
+            while (queue.Count > 0) {
+                var tuple = queue.Dequeue();
+                var res = visitor(tuple.First, tuple.Second);
+                yield return res;
+                foreach (var c in tuple.First._children) {
+                    queue.Enqueue(Pair.Create(c, res));
+                }
+            }
+        }
+
+        public IEnumerable<IReadOnlyTree<T>> DepthFirstVisit()
+        {
+            var stack = new Stack<ReadOnlyTree<T>>();
+            stack.Push(this);
+            while (stack.Count > 0) {
+                var t = stack.Pop();
+                yield return t;
+                foreach (var c in t._children) {
+                    stack.Push(c);
+                }
+            }
+        }
+
+        public void DepthFirstVisit(Action<IReadOnlyTree<T>> visitor)
+        {
+            var stack = new Stack<ReadOnlyTree<T>>();
+            stack.Push(this);
+            while (stack.Count > 0) {
+                var t = stack.Pop();
+                visitor(t);
+                foreach (var c in t._children) {
+                    stack.Push(c);
+                }
+            }
+        }
+
+        public IEnumerable<TRet> DepthFirstVisit<TRet>(Func<IReadOnlyTree<T>, TRet, TRet> visitor, TRet start)
+        {
+            var stack = new Stack<Pair<ReadOnlyTree<T>, TRet>>();
+            stack.Push(Pair.Create(this, start));
+            while (stack.Count > 0) {
+                var tuple = stack.Pop();
+                var res = visitor(tuple.First, tuple.Second);
+                yield return res;
+                foreach (var c in tuple.First._children) {
+                    stack.Push(Pair.Create(c, res));
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[Item: {0}; Children: {1}]", Item, Children.Count());
+        }
+    }
+}
