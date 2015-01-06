63b72a1b26e4bdd95fa84816324318b40e0c0d38
Alessio Parma <alessio.parma@gmail.com>
2015-01-06 10:52:09 +0100

    v2.9.0

--- Examples/HeapSort.cs
+++ Examples/HeapSort.cs
@@ -24,7 +24,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-namespace Hippie.Examples
+namespace Examples
 {
     using System;
     using System.Collections.Generic;
