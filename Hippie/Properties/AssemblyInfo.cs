cd447b266b126033cdad07d4bc66238dce027b87
Alessio Parma <alessio.parma@gmail.com>
2017-03-05 20:24:12 +0100

    v2.10.2

--- Hippie/Properties/AssemblyInfo.cs
+++ Hippie/Properties/AssemblyInfo.cs
@@ -41,7 +41,7 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.10.0")]
-[assembly: AssemblyFileVersion("2.10.1")]
+[assembly: AssemblyFileVersion("2.10.2")]
 
 // Common Language Specification (CLS) compliance generally refers to the claim that CLS rules and
 // restrictions are being followed.
